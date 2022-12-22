1460f25739ff1ad4acda83a3ad2dad6369d2f461
Mohsen Rajabi <m.kabir8895@gmail.com>
2022-12-22 13:50:57 +0330

    update packages

--- src/MediatR.Useful.Behavior/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/MediatR.Useful.Behavior/DependencyInjection/ServiceCollectionExtensions.cs
@@ -4,11 +4,21 @@ using Microsoft.Extensions.DependencyInjection;
 namespace MediatR.Useful.Behavior.DependencyInjection;
 public static class ServiceCollectionExtensions
 {
-
     public static void AddAllBehaviors(this IServiceCollection services)
     {
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheInvalidationBehavior<,>));
+    }
+
+    public static void AddCacheBehaviors(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheInvalidationBehavior<,>));
+    }
+
+    public static void AddValidationBehaviors(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
     }
 }
