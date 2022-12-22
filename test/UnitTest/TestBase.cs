7dae898efe2d7510f3adba905c9dfb6328b96c75
Mohsen Rajabi <m.kabir8895@gmail.com>
2022-12-22 15:30:13 +0330

    add example

--- test/UnitTest/TestBase.cs
+++ test/UnitTest/TestBase.cs
@@ -1,16 +1,9 @@
 ﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using System.Threading.Tasks;
 
 namespace UnitTest;
 public abstract class TestBase
 {
-    protected static ILogger<T> Logger<T>()
-    {
-        return new NullLogger<T>();
-    }
-
     protected Task<TResult> RunBehaviour<TRequest, TResult>(IPipelineBehavior<TRequest, TResult> behaviour, TRequest command)
         where TRequest : IRequest<TResult>
         where TResult : class, new()
