78a9b7ae0aa8aceb310f476b4b0d0a73a067e55c
Mohsen Rajabi <m.kabir8895@gmail.com>
2022-12-24 15:03:05 +0330

    add behavior

---
+++ src/MediatR.Useful.Behavior/Behavior/PerformanceBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR.Useful.Behavior.Behavior;
+public sealed class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly Stopwatch _timer;
+    private readonly ILogger<TRequest> _logger;
+
+    public PerformanceBehaviour(ILogger<TRequest> logger)
+    {
+        _timer = new Stopwatch();
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _timer.Start();
+
+        var response = await next().ConfigureAwait(false);
+
+        _timer.Stop();
+
+        var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+        if (elapsedMilliseconds > 1000)
+        {
+            var requestName = typeof(TRequest).Name;
+
+            _logger.CompileLogStrObj(LogLevel.Warning, "Performance Long Running Request: ",
+                requestName, elapsedMilliseconds, request, null);
+        }
+
+        return response;
+    }
+}
---
+++ src/MediatR.Useful.Behavior/LoggerExt.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace MediatR.Useful.Behavior;
+
+public static partial class LoggerExt
+{
+    [LoggerMessage(
+        EventId = 0,
+        EventName = "CompileLog",
+        Message = "{Message}")]
+    public static partial void CompileLog(this ILogger logger, Exception exp,
+        LogLevel level, string message);
+
+    [LoggerMessage(
+        EventId = 1,
+        EventName = "CompileLogMessage",
+        Message = "{Message}")]
+    public static partial void CompileLogMessage(this ILogger logger,
+        LogLevel level, string message);
+
+    [LoggerMessage(
+        EventId = 2,
+        EventName = "CompileLogStr",
+        Message = "{Message} {Param}")]
+    public static partial void CompileLogStr(this ILogger logger, Exception exp,
+        LogLevel level, string message, string param);
+
+    [LoggerMessage(
+        EventId = 3,
+        EventName = "CompileLogStrStr",
+        Message = "{Message} {Param} {Param2}")]
+    public static partial void CompileLogStrStr(this ILogger logger, Exception exp,
+        LogLevel level, string message, string param, string param2);
+
+    [LoggerMessage(
+        EventId = 4,
+        EventName = "CompileLogObj",
+        Message = "{Message} {Param}")]
+    public static partial void CompileLogObj(this ILogger logger, Exception exp,
+        LogLevel level, string message, object param);
+
+    [LoggerMessage(
+        EventId = 5,
+        EventName = "CompileLogObjObj",
+        Message = "{Message} {Param} {Param2}")]
+    public static partial void CompileLogObjObj(this ILogger logger, Exception exp,
+        LogLevel level, string message, object param, object param2);
+
+
+    [LoggerMessage(
+        EventId = 6,
+        EventName = "CompileLogStrObj",
+        Message = "{Message} {Param} {Param2}")]
+    public static partial void CompileLogStrObj(this ILogger logger, Exception exp,
+        LogLevel level, string message, string param, object param2);
+
+
+    [LoggerMessage(
+        EventId = 7,
+        EventName = "CompileLogMessageStr",
+        Message = "{Message} {Param}")]
+    public static partial void CompileLogMessageStr(this ILogger logger,
+        LogLevel level, string message, string param);
+
+
+
+    [LoggerMessage(
+        EventId = 8,
+        EventName = "CompileLogStrObj",
+        Message = "{Message} {Param} {Param2} {Param3} {Param4}")]
+    public static partial void CompileLogStrObj(this ILogger logger,
+        LogLevel level, string message, object param, object param2, object param3, object param4);
+}
