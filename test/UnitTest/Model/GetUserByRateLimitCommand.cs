7ae01a238e330e422212518dba798874924a9189
Mohammad Amin Pourmoradian <aminf305@yahoo.com>
2024-05-03 18:32:15 +0330

    refactor: add test for rate limit behavior

---
+++ test/UnitTest/Model/GetUserByRateLimitCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MediatR.Useful.Behavior.Model;
+using System;
+
+namespace UnitTest.Model;
+
+public sealed class GetUserByRateLimitCommandReq : IRequest<GetUserByRateLimitCommandRes>,
+     IRateLimitRequest<GetUserByRateLimitCommandRes>
+{
+    public GetUserByRateLimitCommandReq(
+        int permitLimit,
+        Func<GetUserByRateLimitCommandRes, bool> conditionForIncrement,
+        Func<GetUserByRateLimitCommandRes, TimeSpan> conditionWindowTime
+        )
+    {
+        PermitLimit = permitLimit;
+        ConditionForIncrement = conditionForIncrement;
+        ConditionWindowTime = conditionWindowTime;
+    }
+    public string RateLimitCacheKey => $"cacheKey";
+    public int PermitLimit { get; }
+    public Func<GetUserByRateLimitCommandRes, bool> ConditionForIncrement { get; }
+    public Func<GetUserByRateLimitCommandRes, TimeSpan> ConditionWindowTime { get; }
+
+    public bool UseMemoryCache => false;
+}
+public sealed class GetUserByRateLimitCommandRes
+{
+}
